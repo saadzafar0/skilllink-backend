0630cc888bcd7ec119858dfde5957cec9c719205
Saad Zafar <msaadzafar0@gmail.com>
2025-03-22 21:02:57 +0500

    a basic complete layout

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+const { connectDB }  = require ("./config/db")
+
 const express = require ("express")
-const rootRouter = requrie("./routes/index")
+const rootRouter = require("./routes/index")
+
 const PORT = 4000;
 
 const app = express();
@@ -7,7 +10,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/",rootRouter);
-
 
 app.listen(PORT,()=>{
     consol.log(`Server is running on PORT ${PORT}`)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
-require("dotenv").config();
 const express = require('express');
-const userRouter = require("./user")
 
+const userRouter = require("./user")
 const router = express.router()
 
 router.use("/user",userRouter)
-
 
 
 
